95574807769dd0024b54bf1bf3a50ca82e97c204
sinogz <steven.cai@sinodynamic.com>
2016-07-27 19:27:12 +0800

    no message

---
+++ Bike/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bike
+//
+//  Created by sinogz on 16/7/27.
+//  Copyright © 2016年 sinogz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
